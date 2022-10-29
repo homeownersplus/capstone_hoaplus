cc5c3e3d2b26af7eb290454b43bc88682f54d03a
JhasmineEstacio <jhasminejane.estacio.x@bulsu.edu.ph>
2022-10-29 18:34:37 +0800

    aster

--- adminlandingpage.php
+++ adminlandingpage.php
@@ -183,7 +183,7 @@ if (isset($_REQUEST['del'])){
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#">
+                                <a class="dropdown-item" href="admineditprofile.php">
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Profile
                                 </a>
