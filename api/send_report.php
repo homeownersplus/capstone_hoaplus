00d8d3b700cfab9cf73a8de71fe90d0032a3cf53
Frank2899 <aughusf089@gmail.com>
2022-12-12 13:55:11 +0800

    update email config
    update email config

--- api/send_report.php
+++ api/send_report.php
@@ -41,7 +41,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'casseybertman@gmail.com';                      //SMTP username
-    $mail->Password   = 'vfvotethahfzkwfd';                               //SMTP password
+    $mail->Password   = 'fvlybuxxfwogogkx';                               //SMTP password
     $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS
 
